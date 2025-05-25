de610f9000bba820a3904df17ce85878efce57b1
Gaurav garg <gauravthailand@gmail.com>
2025-05-25 09:54:36 +0100

    spawn a zombies after few seconds

--- Assets/Scripts/Authoring/EntitiesReferencesAuthoring.cs
+++ Assets/Scripts/Authoring/EntitiesReferencesAuthoring.cs
@@ -3,6 +3,7 @@ using Unity.Entities;using UnityEngine;
 public class EntitiesReferencesAuthoring : MonoBehaviour
 {
     public GameObject bulletPrefab;
+    public GameObject zombiePrefab;
     
     public class Baker : Baker<EntitiesReferencesAuthoring>
     {
@@ -11,7 +12,8 @@ public class EntitiesReferencesAuthoring : MonoBehaviour
             Entity entity = GetEntity(TransformUsageFlags.Dynamic);
             AddComponent(entity, new EntitiesReferences
             {
-                bulletPrefabEntity = GetEntity(authoring.bulletPrefab, TransformUsageFlags.Dynamic)
+                bulletPrefabEntity = GetEntity(authoring.bulletPrefab, TransformUsageFlags.Dynamic),
+                zombiePrefabEntity = GetEntity(authoring.zombiePrefab, TransformUsageFlags.Dynamic)
             });
         }
     }
@@ -20,4 +22,5 @@ public class EntitiesReferencesAuthoring : MonoBehaviour
 public struct EntitiesReferences : IComponentData
 {
     public Entity bulletPrefabEntity;
+    public Entity zombiePrefabEntity;
 }
--- Assets/Scripts/Systems/UnitMoverSystem.cs
+++ Assets/Scripts/Systems/UnitMoverSystem.cs
@@ -69,10 +69,10 @@ public partial struct UnitMoverJob : IJobEntity
     /// </summary>
     public void Execute(ref LocalTransform localTransform, in UnitMover unitMover, ref PhysicsVelocity physicsVelocity)
     {
-        if(unitMover.targetPosition.Equals(float3.zero))
-        {
-            return;
-        }
+        // if(unitMover.targetPosition.Equals(float3.zero))
+        // {
+        //     return;
+        // }
         
         float3 targetPosition = unitMover.targetPosition;
         float3 moveDirection = targetPosition - localTransform.Position;
